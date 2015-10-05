fdb2f85e8ce2fbd6082aefe23efd0db1e85cffde
yang-xu <yang.xu@dena.com>
2015-10-05 21:14:13 +0900

    fix command for calc

--- kuma-bot/commands/calc.rb
+++ kuma-bot/commands/calc.rb
@@ -3,19 +3,20 @@ require 'dentaku'
 module KumaBot
   module Commands
     class Calc < SlackRubyBot::Commands::Base
-      operator '='
-      command 'calculate'
-
-      def self.call(client, data, match)
-        result = Dentaku::Calculator.new.evaluate(match[:expression]) if match.names.include?('expression')
-        result = result.to_s if result
-        if result && result.length > 0
-          send_message client, data.channel, result
-        else
-          send_message_with_gif client, data.channel, 'I got nothing...'
+      match(/^kumakun =(?<category>.+?)\s+(?<expression>.+)$/) do |client, data, match|
+        begin
+          expression = match['expression'].strip
+          result = Dentaku::Calculator.new.evaluate(expression)
+          result = result.to_s if result
+          if result && result.length > 0
+            send_message client, data.channel, result
+          else
+            send_message_with_gif client, data.channel, 'I got nothing...'
+          end
+        raise StandardError
+        rescue StandardError => e
+          send_message client, data.channel, "I got \"#{e.message}\". Check your expression again?"
         end
-      rescue StandardError => e
-        send_message client, data.channel, "I got \"#{e.message}\". Check your expression again?"
       end
     end
   end
