fba4689bde8284c37322c9722210f4b26e10e19e
yang-xu <yang.xu@dena.com>
2015-10-21 19:20:49 +0900

    fix

--- kuma-bot/commands/tabelog.rb
+++ kuma-bot/commands/tabelog.rb
@@ -30,7 +30,7 @@ module KumaBot
           search_url = "http://tabelog.com/#{station_code}/rstLst/1/?SrtT=rt&sk=#{query}"
           html = `curl #{search_url}`
           if html =~ /全 <span class="text-num fs15"><strong>(\d+)<\/strong><\/span> 件/
-            if $1 > 1200
+            if $1.to_i > 1200
               max_page = 60
             else
               max_page = ($1.to_f / 20).ceil
