df718da79efd4b17db03aecfbdc2b041866e6978
yang-xu <yang.xu@dena.com>
2015-10-21 19:28:07 +0900

    fix

--- kuma-bot/commands/tabelog.rb
+++ kuma-bot/commands/tabelog.rb
@@ -37,7 +37,7 @@ module KumaBot
             end
           end
 
-	  puts max_page
+	  send_message client, data.channel, "#{max_page}"
           restaurant_links = Array.new
           (1..max_page.to_i).each do |page|
             search_url = "http://tabelog.com/#{station_code}/rstLst/#{page}/?SrtT=rt&sk=#{query}"
