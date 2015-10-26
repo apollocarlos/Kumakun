915c112e494b71fb2f8f503b18888d44183c52c6
yang-xu <yang.xu@dena.com>
2015-10-26 18:59:18 +0900

    fix

--- kuma-bot/commands/tabelog.rb
+++ kuma-bot/commands/tabelog.rb
@@ -74,13 +74,13 @@ module KumaBot
             url = restaurant_links.delete_at(idx)
             info = parse_url(url, proxy)
             send_message client, data.channel, "
-	    ================================================
-	    *#{info["name"]}*
-	    Genre: #{info["genre"]}
-	    Rate: #{info["rate"]}
-	    Address: #{info["addr"]}
-	    Webpage: #{info["url"]}
-	    "
+================================================
+*#{info["name"]}*
+  Genre: #{info["genre"]}
+  Rate: #{info["rate"]}
+  Address: #{info["addr"]}
+  Webpage: #{info["url"]}
+            "
           end
 
         # location doesn't match, but we can guess
