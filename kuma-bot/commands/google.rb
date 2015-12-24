43496b9c859921c8fafd44e0cf5289d02b277cc6
yang-xu <yang.xu@dena.com>
2015-12-24 18:31:29 +0900

    fix

--- kuma-bot/commands/google.rb
+++ kuma-bot/commands/google.rb
@@ -28,14 +28,12 @@ module KumaBot
 
       def self.handle_google_knowledge_graph(expression, client, data)
         if expression =~ /(.+)\s+--([a-zA-Z]{2})/
-          query = $1.gsub(/ /, '+')
           language = $2
+          query = $1.gsub(/ /, '+')
         else
-          query = expression.gsub(/ /, '+')
           language = "en"
+          query = expression.gsub(/ /, '+')
         end
-        request = "https://kgsearch.googleapis.com/v1/entities:search?query=#{query}&key=#{API}&limit=1&indent=True&languages=#{language}"
-	send_message client, data.channel, "#{query}::::#{language}:::#{request}"
         response = `curl 'https://kgsearch.googleapis.com/v1/entities:search?query=#{query}&key=#{API}&limit=1&indent=True&languages=#{language}'`
         result = JSON.parse(response)
         item = result["itemListElement"][0]
